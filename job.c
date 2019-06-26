8626f17b354e6b778a6a8fbdfb81893c22c39625
JannetAbd <abdelkefi.jannet@gmail.com>
2019-06-26 05:56:38 -0400

    add job job.c

---
+++ job.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int const a=50 , b= 0 ;
+
+
+ int g(){
+int q; 
+q=a/b; 
+return q ;}
+
+int main() {
+
+//jhsdi
+printf("\n this is a test\n %i/%i=%d\nEnd of test\n",g()) ;
+return 0 ; 
+
+}
+
